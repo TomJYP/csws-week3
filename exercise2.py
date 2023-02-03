148beb184418e7f91ce75cdea5c687c73ec2c692
Sameer Naeem <sameernm2003@outlook.com>
2023-02-03 12:12:00 +0000

    initial commit

---
+++ exercise2.py
@@ -0,0 +1,19 @@
+num_list = list(range(1, 1000001))
+#print(num_list)
+
+#print()
+
+print(min(num_list))
+print(max(num_list))
+print(sum(num_list))
+
+print()
+
+for x in range(1, 20, 2):
+    print(x)
+
+print()
+
+for x in range(3, 33, 3):
+    print(x)
+print("End of the list")
